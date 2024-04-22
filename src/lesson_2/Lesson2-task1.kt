4948e67ddded2c6d6cfbd2a37d8aef94b07b8440
Grigoriy <grigoriy.nenkov@mail.ru>
2024-04-22 20:42:30 +0300

    Ks-2-1

---
+++ src/lesson_2/Lesson2-task1.kt
@@ -0,0 +1,10 @@
+package lesson_2
+
+fun main(){
+    val mark1 = 3
+    val mark2 = 4
+    val mark3 = 5
+    val summNums = 2*mark1 + mark2 + mark3
+    val averageValue: Float= summNums/4f
+    println(averageValue)
+}
