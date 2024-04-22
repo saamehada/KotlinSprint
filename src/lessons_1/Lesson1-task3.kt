93c8a365f26f29b6a4da11b982f291a1e0c32e25
Grigoriy <grigoriy.nenkov@mail.ru>
2024-04-22 18:41:36 +0300

    Ks-1-3

---
+++ src/lessons_1/Lesson1-task3.kt
@@ -0,0 +1,14 @@
+package lessons_1
+
+fun main(){
+    val year: Int = 1961
+    var hour: Int = 9
+    var minute: Int = 7
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = 10
+    minute = 55
+    println("$hour:$minute")
+}
