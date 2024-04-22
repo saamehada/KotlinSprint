98990fa2f09c03de1193ea665ea09cacea6840c3
Grigoriy <grigoriy.nenkov@mail.ru>
2024-04-22 17:23:07 +0300

    Ks-1-1

---
+++ src/lessons_1/Lesson1.kt
@@ -0,0 +1,8 @@
+package lessons_1
+
+fun main() {
+    val numbersOfOrders: Byte = 75
+    val thanksForThePurchase: String = "У вас прекрасный вкус! Спасибо за покупку!!!"
+    println(numbersOfOrders)
+    println(thanksForThePurchase)
+}
