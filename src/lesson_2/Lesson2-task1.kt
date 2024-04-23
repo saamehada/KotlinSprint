9167c9df89f4851e25cfc124b692ac13c3dce127
Grigoriy <grigoriy.nenkov@mail.ru>
2024-04-23 20:11:22 +0300

    Ks--3-1

--- src/lesson_2/Lesson2-task1.kt
+++ src/lesson_2/Lesson2-task1.kt
@@ -1,10 +1,11 @@
 package lesson_2
 
-fun main(){
+fun main() {
     val mark1 = 3
     val mark2 = 4
     val mark3 = 5
-    val summNums = 2*mark1 + mark2 + mark3
-    val averageValue: Float= summNums/4f
-    println(averageValue)
+    val students = 4.0
+    val summNums = 2 * mark1 + mark2 + mark3
+    val averageValue = summNums / students
+    println("%.2f".format(averageValue))
 }
