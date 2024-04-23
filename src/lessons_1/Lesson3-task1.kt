6df722bc292d1daa9fba44f5fccd775fd36353d6
Grigoriy <grigoriy.nenkov@mail.ru>
2024-04-23 19:21:17 +0300

    Ks-3-1

---
+++ src/lessons_1/Lesson3-task1.kt
@@ -0,0 +1,11 @@
+package lessons_1
+
+fun main(){
+    val nameHuman = "Святослав"
+    var greeting = "С добрым утром!"
+    println("$greeting, $nameHuman")
+
+
+    greeting = "Добрый вечер!"
+    println("$greeting, $nameHuman")
+}
