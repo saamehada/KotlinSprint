0b6fb844ca91b551511d07110f52044139c55d02
Grigoriy <grigoriy.nenkov@mail.ru>
2024-04-23 15:34:07 +0300

    Ks-2-4

---
+++ src/lessons_1/Lesson2-task4.kt
@@ -0,0 +1,12 @@
+package lessons_1
+
+fun main(){
+    val crystalOre = 7
+    val ironOre = 11
+    val buff = 20
+    val buffCrystalOre = (buff * crystalOre) / 100
+    val buffIronOre = (buff * ironOre) / 100
+
+    println(buffCrystalOre)
+    println(buffIronOre)
+}
