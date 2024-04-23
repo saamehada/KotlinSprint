a7b7d5a1a9c4e2a7c95b3958a7c585096df78e5c
Grigoriy <grigoriy.nenkov@mail.ru>
2024-04-23 17:57:09 +0300

    Ks-1-3

--- src/lessons_1/Lesson1-task3.kt
+++ src/lessons_1/Lesson1-task3.kt
@@ -2,13 +2,13 @@ package lessons_1
 
 fun main(){
     val year: Int = 1961
-    var hour: Int = 9
-    var minute: Int = 7
+    var hour: String = "09"
+    var minute: String = "07"
     println(year)
     println(hour)
     println(minute)
 
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
     println("$hour:$minute")
 }
