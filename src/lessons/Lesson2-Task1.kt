55ccf3f287dd615e8a7379351f3898d810e014c8
Grigoriy <grigoriy.nenkov@mail.ru>
2024-04-24 19:09:38 +0300

    KS-2-1
    Signed-off-by: Grigoriy <grigoriy.nenkov@mail.ru>

--- src/lessons/Lesson2-Task1.kt
+++ src/lessons/Lesson2-Task1.kt
@@ -4,8 +4,9 @@ fun main() {
     val mark1 = 3
     val mark2 = 4
     val mark3 = 5
-    val students = 4.0
+    val students = 4
     val summNums = 2 * mark1 + mark2 + mark3
-    val averageValue = summNums / students
+    val summNumsFloat = summNums.toFloat()
+    val averageValue = summNumsFloat / students
     println("%.2f".format(averageValue))
 }
