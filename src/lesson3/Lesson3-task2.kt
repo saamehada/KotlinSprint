8dcc8c65e58a326d8c8bb2f53d637a8c4aa73e73
Grigoriy <grigoriy.nenkov@mail.ru>
2024-04-24 10:54:46 +0300

    KS-3-2
    Signed-off-by: Grigoriy <grigoriy.nenkov@mail.ru>

---
+++ src/lesson3/Lesson3-task2.kt
@@ -0,0 +1,13 @@
+package lesson3
+
+fun main() {
+    val name = "Татьяна"
+    val patronymic = "Сергеевна"
+    var surname = "Андреева"
+    var age = 20
+    println("$surname $name $patronymic, $age")
+
+    surname = "Сидорова"
+    age = 22
+    println("$surname $name $patronymic, $age")
+}
