010c618ecd3356e5fb882d18a38c1d2a5f1e295f
Grigoriy <grigoriy.nenkov@mail.ru>
2024-04-24 19:15:36 +0300

    KS-2-3
    Signed-off-by: Grigoriy <grigoriy.nenkov@mail.ru>

--- src/lesson_2/Lesson2-task3.kt
+++ src/lesson_2/Lesson2-task3.kt
@@ -4,13 +4,17 @@ fun main(){
     val hourDeparture = 9
     val minuteDeparture = 39
     val trainPathMinute = 457
-    val arrivalHours = trainPathMinute / 60
-    val partRemainder = arrivalHours * 60
+    val numbersInHour = 60
+
+
+    val arrivalHours = trainPathMinute / numbersInHour
+    val partRemainder = arrivalHours * numbersInHour
     val remainingMinutes = minuteDeparture + (trainPathMinute - partRemainder)
     val arrivalOfTheTrainHours = hourDeparture + arrivalHours
-    val timeMinute = remainingMinutes / 60
+    val timeMinute = remainingMinutes / numbersInHour
     val mainTimeHour = timeMinute + arrivalOfTheTrainHours
-    val mainTineMinutes = remainingMinutes % 60
+    val mainTineMinutes = remainingMinutes % numbersInHour
+
 
     println("$mainTimeHour:$mainTineMinutes")
 }
