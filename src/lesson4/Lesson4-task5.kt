2b1348ad9eccf08e67f69ed31dc1501eef515383
Grigoriy <grigoriy.nenkov@mail.ru>
2024-04-30 23:46:48 +0300

    KS-4-5
    Signed-off-by: Grigoriy <grigoriy.nenkov@mail.ru>

--- src/lesson4/Lesson4-task5.kt
+++ src/lesson4/Lesson4-task5.kt
@@ -1,16 +1,26 @@
 package lesson4
 
 fun main() {
+    println("В качестве входных данных используй информацию, введенную через консоль: ")
+
+    println("Введите, есть ли повреждение у корабля")
     val damageOfTheShip = readln().toBoolean()
-    val numbersPeople = readln()!!.toInt()
-    val numbersBox = readln()!!.toInt()
+
+    println("Введите число экипажа")
+    val numbersPeople = readln().toInt()
+
+    println("Введите количество ящиков провизии")
+    val numbersBox = readln().toInt()
+
+    println("Введите благоприятность погоды для плавания (благоприятная или неблагоприятная)")
     val weatherForShip = readln().toBoolean()
 
 
-    val mainAnswer = (damageOfTheShip == DAMAGE_SHIP) && ((numbersPeople <= NUMBERS_PEOPLE_MAX) && (numbersPeople >= NUMBERS_PEOPLE_MIN))
-            && (numbersBox >= NUMBERS_BOXES) && ((weatherForShip == WEATHER_TODAY) || (weatherForShip == WEATHER_TODAY2))
-            && (damageOfTheShip == DAMAGE_SHIP2) && ((weatherForShip == WEATHER_TODAY) || (weatherForShip == WEATHER_TODAY2))
-            && (numbersBox >= NUMBERS_BOXES) && (numbersPeople == NUMBERS_PEOPLE_MAX)
+    val mainAnswer =
+        ((damageOfTheShip == DAMAGE_SHIP) && ((numbersPeople <= NUMBERS_PEOPLE_MAX) && (numbersPeople >= NUMBERS_PEOPLE_MIN)) &&
+                (numbersBox >= NUMBERS_BOXES) && (weatherForShip == WEATHER_TODAY)) ||
+                ((damageOfTheShip != DAMAGE_SHIP) && (weatherForShip != WEATHER_TODAY) &&
+                        (numbersBox >= NUMBERS_BOXES) && (numbersPeople == NUMBERS_PEOPLE_MAX))
 
 
     println(mainAnswer)
@@ -21,6 +31,4 @@ const val NUMBERS_PEOPLE_MAX = 70
 const val NUMBERS_BOXES = 50
 
 const val DAMAGE_SHIP: Boolean = false
-const val DAMAGE_SHIP2: Boolean = true
 const val WEATHER_TODAY: Boolean = false
-const val WEATHER_TODAY2: Boolean = true
